506794df911b7a5c2e1c24c598989e4355752a41
Ganiyu_Omotayo Seedik <tayorgudestryhero@gmail.com>
2020-08-30 23:52:47 +0100

    Redesign

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
